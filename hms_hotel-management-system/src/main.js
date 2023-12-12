22f5f0e2170e3aedb94ebd8088dcbe2b8de97200
FaustXL <143204667+FaustXL@users.noreply.github.com>
2023-12-12 08:19:53 +0800

    优化配置文件

--- hms_hotel-management-system/src/main.js
+++ hms_hotel-management-system/src/main.js
@@ -1,41 +1,3 @@
-/* import axios from 'axios'
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-import { far } from '@fortawesome/free-regular-svg-icons'
-
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import '@/views/css/nav.css'
-
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText }
- from '@fortawesome/vue-fontawesome'
-library.add(fas, far, fab)
-
-axios.defaults.baseURL="/api" //
-createApp(App).use(router).mount('#app')
- 
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.component('font-awesome-layers', FontAwesomeLayers)
-
-Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
-
-Vue.config.productionTip = false
-Vue.use(ElementUI);
-
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
-}) */
 import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
@@ -53,7 +15,6 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import '@/views/css/nav.css'
 
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
-
 
 library.add(fas, far, fab)
 
